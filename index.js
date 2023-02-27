124e0deefd3e9b24f6bd4a031035948088ec746e
SalemAbdullah30 <skyline3291@hotmail.com>
2023-02-27 07:26:07 +0300

    I add buttons and audio 'same audio for testing'. I also made it responsive through JavaScript

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const audioPlayer = document.getElementById('audio-player');
-const playPauseButton = document.getElementById('play-pause-button');
+const audioPlayers = document.querySelectorAll('.audio-player');
+const playPauseButtons = document.querySelectorAll('.play-pause-button');
 
 let isPlaying = false;
 
-playPauseButton.addEventListener('click', () => {
-  if (isPlaying) {
-    audioPlayer.pause();
-    playPauseButton.innerText = 'Play';
-  } else {
-    audioPlayer.play();
-    playPauseButton.innerText = 'Pause';
-  }
-  isPlaying = !isPlaying;
-  console.log(isPlaying);
+playPauseButtons.forEach((playPauseButton, index) => {
+  const audioPlayer = audioPlayers[index].querySelector('audio');
+
+  playPauseButton.addEventListener('click', () => {
+    if (isPlaying) {
+      audioPlayer.pause();
+      playPauseButton.innerText = 'Play';
+    } else {
+      audioPlayer.play();
+      playPauseButton.innerText = 'Pause';
+    }
+    isPlaying = !isPlaying;
+  });
 });
